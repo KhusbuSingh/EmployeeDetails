c445902165d269fadd5fd4b9f5b84249f472c623
KhusbuSingh <skhusbu050@gmail.com>
2023-10-25 15:48:19 +0530

    new code

---
+++ src/com/hcl/Employee.java
@@ -0,0 +1,39 @@
+package com.hcl;
+
+public class Employee {
+		private void empId() {
+			System.out.println("emp id is 1001");
+		 System.out.println("subemployee id is 3377");
+		}
+		private void empNAME() {
+			System.out.println("emp name is saroj");
+	    System.out.println("subemployee name is khushi");
+		}
+		private void empDob() {
+			System.out.println("emp dob is 02-02-1979");
+	  System.out.println("subemp dob id 03-07-97");
+		}
+		private void empPhone() {
+			System.out.println("emp phone is 123456");
+			
+
+		}
+		private void empEmail() {
+			System.out.println("Employee1");
+			System.out.println("Employee2");
+			
+
+		}
+		public static void main(String[] args) {
+			Employee e= new Employee();
+			e.empId();
+			e.empNAME();
+			e.empDob();
+			e.empPhone();
+			e.empEmail();
+			
+		}
+		
+
+	}
+
